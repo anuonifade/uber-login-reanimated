80e4bf0dd7697eb0d09634abd803b1ad718ba4ee
andela-aonifade <anuoluwapo.onifade@andela.com>
2020-06-10 05:16:08 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Animated, { useCode, cond, eq, set } from 'react-native-reanimated';
+import { withTimingTransition } from 'react-native-redash';
+
+import Logo from './components/Logo';
 
 export default function App() {
+
+  const scale = useRef(new Animated.Value(0));
+  const scaleAnimation = withTimingTransition(scale.current);
+
+  useCode(() => cond(eq(scale.current, 0), set(scale.current, 1)), []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.logoContainer}>
+        <Logo scale = {scaleAnimation}/>
+      </View>
     </View>
   );
 }
@@ -12,8 +23,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2289d6',
+  },
+  logoContainer: {
+    flex:1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
